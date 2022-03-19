5656d3d22c49adbb5f3ac227c596889c64e723aa
dksxorb1 <xorb0909@naver.com>
2022-03-20 00:59:23 +0900

    added Star.java

---
+++ src/Star.java
@@ -0,0 +1,43 @@
+
+public class Star {
+
+	public static void main(String[] args) {
+		int i;
+		int j;
+	 for(i=1;i<9; i++) {
+		 for(j=2; j<10; j++) {
+			 System.out.print(" "+i +"*"+ j +"="+ i*j );
+		 }
+	if(i==2) {
+		System.out.println(i +"*"+ j +"="+ i*j );	
+	} 
+	else if(i==3) {
+		System.out.println(i +"*"+ j +"="+ i*j );	
+	}
+	else if(i==4) {
+		System.out.println(i +"*"+ j +"="+ i*j );			 
+			 
+		 }
+	else if(i==5) {
+		System.out.println(i +"*"+ j +"="+ i*j );	 
+	      }
+	else if(i==6) {
+		System.out.println(i +"*"+ j +"="+ i*j );
+	   }
+	else if(i==7) {
+		System.out.println(i +"*"+ j +"="+ i*j );
+		}
+	else if(i==8) {
+		System.out.println(i +"*"+ j +"="+ i*j ); 
+	}
+	else if(i==9) {
+		System.out.println(i +"*"+ j +"="+ i*j );
+	}
+	 }
+	}
+}
+		
+		
+		
+		
+		
